04f5c20ee5198292d4b4f42d80a81fe7a448b8b6
Pawel Bogut <pbogut@smeagol.pl>
2016-09-30 23:01:06 +0100

    Basic implementation

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # mails-go-web
 
-[![Project Status: Concept - Minimal or no implementation has been done yet.](http://www.repostatus.org/badges/latest/concept.svg)](http://www.repostatus.org/#concept)
+[![Project Status: WIP - Initial development is in progress, but there has not yet been a stable, usable release suitable for the public.](http://www.repostatus.org/badges/latest/wip.svg)](http://www.repostatus.org/#wip)
 
 Mails Go Web - HTTP server for rendering mails from Maildir files.
---
+++ mails_go_web.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/veqryn/go-email/email"
+)
+
+func download(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Not implemented yet...")
+}
+
+func view(w http.ResponseWriter, r *http.Request) {
+	if len(r.URL.Query()["q"]) == 0 {
+		io.WriteString(w, "Query not provided.")
+		return
+	}
+	query := r.URL.Query()["q"][0]
+	if _, err := os.Stat(query); os.IsNotExist(err) {
+		io.WriteString(w, "Email not found.")
+		return
+	}
+
+	file, _ := ioutil.ReadFile(query)
+	reader := strings.NewReader(string(file))
+	msg, _ := email.ParseMessage(reader)
+	for _, part := range msg.MessagesAll() {
+		mediaType, _, _ := part.Header.ContentType()
+		switch mediaType {
+		case "text/html":
+			io.WriteString(w, string(part.Body))
+		case "text/plain":
+			io.WriteString(w, string(part.Body))
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/", view)
+	http.HandleFunc("/attachment", download)
+
+  fmt.Println("Sstarting server on http://localhost:6245")
+	http.ListenAndServe("localhost:6245", nil)
+}
