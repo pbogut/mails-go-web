ec10e2b4fedd00e5db062f9a54850e622b12a13e
Pawel Bogut <pbogut@smeagol.pl>
2016-10-02 15:12:15 +0100

    Rename script

--- scripts/mail-to-web.sh
+++ scripts/mail-to-web.sh
@@ -5,5 +5,5 @@ else
     email_content="$1"
 fi
 
-query=`cat "$email_content" | grep '^Message-ID: ' | sed 's#^Message-ID: <\(.*\)>#\1#g'`
+query=`cat "$email_content" | grep '^Message-ID: ' | sed 's#^Message-ID: <\(.*\)>#\1#g' | ruby -ne 'require "cgi"; puts CGI.escape($_)'`
 chromium  "http://localhost:6245?q=$query"
