dc891dfe833f58ce972933a1fa5224d395d345e6
Pawel Bogut <pbogut@pbogut.me>
2018-12-20 00:05:22 +0100

    Add header to viewed message

--- mails_go_web.go
+++ mails_go_web.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
+	"html"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"os/exec"
+	"regexp"
 	"strings"
 
 	"github.com/jessevdk/go-flags"
@@ -45,7 +48,7 @@ func query_to_file(q string) string {
 	return file_path
 }
 
-func get_email_view(file_path string) string {
+func get_email_body(file_path string) string {
 	var body string
 	file, _ := ioutil.ReadFile(file_path)
 	reader := strings.NewReader(string(file))
@@ -67,6 +70,126 @@ func get_email_view(file_path string) string {
 	return body
 }
 
+func get_email_view(file_path string, url string) string {
+	file, _ := ioutil.ReadFile(file_path)
+	reader := strings.NewReader(string(file))
+	msg, _ := email.ParseMessage(reader)
+
+	body := `<!doctype html>
+	<html>
+		<head>
+			<style>
+			body, html {
+				height: 100%%;
+				margin: 0;
+				padding: 0;
+				overflow: hidden;
+			}
+			.container {
+				margin: auto;
+				max-width: 800px;
+				height: 100%%;
+				border: 1px solid black;
+				box-sizing: border-box;
+			}
+			.header {
+				font-size: 14px;
+				height: 84px;
+				box-sizing: border-box;
+			}
+			.header img {
+				float: left;
+			}
+			.header table {
+				width: 100%%;
+			}
+			.header td.gravatar {
+				width: 80px;
+			}
+			.header td.field-name {
+				padding-right: 5px;
+				width: 70px;
+				font-weight: bold;
+				text-align: right;
+			}
+			.header td.bd-r {
+				border-right: 1px solid black;
+			}
+			.header td.bd-b {
+				border-bottom: 1px solid black;
+			}
+			iframe {
+				height: calc(100%% - 84px);
+				width: 100%%;
+			}
+			</style>
+		</head>
+		<body>
+			<div class="container">
+				<div class="header">
+				<table cellspacing="0" border="0">
+					<tr>
+						<td rowspan="4" class="bd-b bd-r gravatar">
+							<img src="http://www.gravatar.com/avatar/%x?s=80&d=identicon" alt="" />
+						</td>
+						<td class="field-name">
+							From:
+						</td>
+						<td class="bd-r">
+							%s
+						</td>
+					</tr>
+					<tr>
+						<td class="field-name">
+							To:
+						</td>
+						<td class="bd-r">
+							%s
+						</td>
+					</tr>
+					<tr>
+						<td class="field-name">
+							Date:
+						</td>
+						<td class="bd-r">
+							%s
+						</td>
+					</tr>
+					<tr>
+						<td class="field-name bd-b">
+							Subject:
+						</td>
+						<td class="bd-b bd-r">
+							%s
+						</td>
+					</tr>
+				</table>
+				</div>
+				<iframe frameborder="0" src="%s">
+				</iframe>
+			</div>
+		</body>
+	</html>
+	`
+
+	re := regexp.MustCompile("[^<]*<(.*)>")
+	from := strings.ToLower(re.ReplaceAllString(msg.Header.From(), "$1"))
+	data := []byte(from)
+	hash := md5.Sum(data)
+
+	date, _ := msg.Header.Date()
+
+	return fmt.Sprintf(
+		body,
+		hash,
+		html.EscapeString(msg.Header.From()),
+		msg.Header.To(),
+		date.Format("Mon, 2 Jan [2006-01-02 15:04:05]"),
+		html.EscapeString(msg.Header.Subject()),
+		url,
+	)
+}
+
 func download(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Not implemented yet...")
 }
@@ -83,7 +206,12 @@ func view(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	body := get_email_view(file_path)
+	var body string
+	if len(r.URL.Query()["m"]) == 0 {
+		body = get_email_view(file_path, r.URL.Path+"?m=true&"+r.URL.RawQuery)
+	} else {
+		body = get_email_body(file_path)
+	}
 	// render body
 	io.WriteString(w, body)
 }
